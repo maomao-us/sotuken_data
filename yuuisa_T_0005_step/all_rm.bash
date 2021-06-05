e55e75ec5ee116fe3c13a839d722b9343e62d4ab
maomao-us <79571542+maomao-us@users.noreply.github.com>
2021-06-05 18:56:56 +0900

    Add files via upload

---
+++ yuuisa_T_0005_step/all_rm.bash
@@ -0,0 +1,47 @@
+cd inoue
+
+rm *.csv
+
+cd ..
+
+cd kawamura
+
+rm *.csv
+
+cd ..
+
+
+cd kisimoto
+
+rm *.csv
+
+cd ..
+
+
+cd kutukake
+
+rm *.csv
+
+cd ..
+
+cd takenaka
+
+rm *.csv
+
+cd ..
+
+cd horinouti
+
+rm *.csv
+
+cd ..
+
+cd matui
+
+rm *.csv
+
+cd ..
+
+cd yamanada
+
+rm *.csv
